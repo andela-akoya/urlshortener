2aa10cf1cdba6e475b30df60ec48ccbaf26a044e
koya adegboyega <adegboyega.koya@andela.com>
2017-04-03 12:45:25 +0100

    [Chore] adds comments to test_basic file

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -19,7 +19,12 @@ class BasicsTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_app_exist(self):
+        """tests whether a false return is gotten for a non existing app"""
         self.assertFalse(current_app is None)
 
     def test_app_is_testing(self):
+        """
+        tests the configuration setup if it returns true if an application is
+        created 
+        """
         self.assertTrue(current_app.config["TESTING"])
