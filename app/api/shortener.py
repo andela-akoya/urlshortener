2187db9708fbcc9a1f4b046b9d8c4c0a976f3d46
koya adegboyega <adegboyega.koya@andela.com>
2017-04-03 18:18:47 +0100

    [Feature] Implements string generator Adds a function that generates a string randomly

---
+++ app/api/shortener.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import random, string, uuid
+
+
+class Shortener:
+    @staticmethod
+    def generate_shorten_name(length_of_short_url):
+        """
+        generates a  random string having a length equal to the integer
+        argument (length_of_short_url parameter)
+        """
+
+        short_url_length = length_of_short_url if length_of_short_url else 6
+        characters = string.ascii_uppercase + string.digits \
+                     + string.ascii_lowercase \
+                     + str(uuid.uuid4()).replace("-", "")
+
+        return ''.join(random.SystemRandom().choice(characters)
+                       for _ in range(short_url_length))
