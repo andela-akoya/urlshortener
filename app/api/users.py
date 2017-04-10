b97f13232500f5fa9b52a96b609244d6a984516f
koya adegboyega <adegboyega.koya@andela.com>
2017-04-10 09:22:15 +0100

    [Feature] Implements an activation toggle endpoint

--- app/api/users.py
+++ app/api/users.py
@@ -1,14 +1,12 @@
 # coding=utf-8
 from flask import jsonify, request, g
-from sqlalchemy import desc
-from validators import ValidationFailure
 from werkzeug.exceptions import NotFound
 
 from . import api
 from .authentication import auth
 from .custom_exceptions import ValidationException, UrlValidationException
 from .decorators import permission, admin_permission
-from .errors import validation_error, page_not_found
+from .errors import page_not_found, custom_error
 from .utilities import Utilities
 from app.models import Url, ShortenUrl, AnonymousUser
 
@@ -40,25 +38,36 @@ def generate_shorten_url():
 
 
 @api.route('/api/urls')
+@api.route('/api/urls/popularity')
 @auth.login_required
-@admin_permission
 def get_urls():
     """ returns a list of all the long urls ordered by date of creation """
-    url_list = Url.query.order_by(desc(Url.date_added)).all()
+    url_list = Url.get_all_urls_by_dated_added()
     return jsonify(
-        [Utilities.to_json(url, ['id', 'url_name', 'date_added']) for url in url_list]
+        {
+            "url_list": [
+                Utilities.to_json(url, ['id', 'url_name', 'date_added'])
+                for url in url_list
+            ]
+        }
+
     )
 
 
 @api.route('/api/shorten-urls')
+@api.route('/api/shorten-urls/popularity')
 @auth.login_required
-@admin_permission
 def get_shorten_urls():
     """ returns a list of all the shorten urls ordered by date of creation """
-    shorten_url_list = ShortenUrl.query.order_by(desc(ShortenUrl.date_added)).all()
-    return jsonify([Utilities.to_json(
-        shorten_url, ['id', 'url_name', 'date_added'])
-                    for shorten_url in shorten_url_list]
+    shorten_url_list = ShortenUrl.get_all_shorten_urls_by_dated_added()
+    return jsonify(
+        {
+            "shorten_url_list": [
+                Utilities.to_json(shorten_url, ['id', 'url_name', 'date_added',
+                                                'is_active', 'deleted'])
+                for shorten_url in shorten_url_list
+            ]
+        }
     )
 
 
@@ -91,7 +100,12 @@ def get_urls_for_particular_user():
     AnonymousUser.set_anonymous()
     url_list = g.current_user.url_list
     return jsonify(
-        [Utilities.to_json(url, ['id', 'url_name']) for url in url_list]
+        {
+            "url_list": [
+                Utilities.to_json(url, ['id', 'url_name']) for url in url_list
+            ]
+        }
+
     )
 
 
@@ -105,10 +119,14 @@ def get_short_urls_for_particular_user():
     AnonymousUser.set_anonymous()
     short_url_list = g.current_user.short_url_list
     return jsonify(
-        [
-            Utilities.to_json(short_url, ['id', 'shorten_url_name'])
-            for short_url in short_url_list
-        ]
+        {
+            "shorten_url_list": [
+                Utilities.to_json(short_url, ['id', 'shorten_url_name',
+                                              'date_added', 'is_active',
+                                              'deleted'])
+                for short_url in short_url_list
+            ]
+        }
     )
 
 
@@ -119,8 +137,13 @@ def get_long_url_with_shorten_url_id(id):
     is passed as argument
     """
     try:
-        long_url = Url.query.get_or_404(
-            ShortenUrl.query.get_or_404(id).long_url)
+        shorten_url_object = ShortenUrl.query.get_or_404(id)
+        if shorten_url_object.deleted:
+            return custom_error("The shorten url has been deleted", "deleted")
+        elif not shorten_url_object.is_active:
+            return custom_error("The shorten url has been deactivated",
+                                "inactive")
+        long_url = Url.query.get_or_404(shorten_url_object.long_url)
         return jsonify(Utilities.to_json(long_url, ['id', 'url_name',
                                                     'date_added']))
     except NotFound:
@@ -133,8 +156,13 @@ def get_long_url_with_shorten__url_name(shorten_url_name):
     is passed as argument
     """
     try:
-        shorten_url_object= ShortenUrl.query.filter_by(
+        shorten_url_object = ShortenUrl.query.filter_by(
             shorten_url_name=shorten_url_name).first_or_404()
+        if shorten_url_object.deleted:
+            return custom_error("The shorten url has been deleted", "deleted")
+        elif not shorten_url_object.is_active:
+            return custom_error("The shorten url has been deactivated",
+                                "inactive")
         long_url = Url.query.get_or_404(shorten_url_object.long_url)
         return jsonify(Utilities.to_json(long_url, ['id', 'url_name',
                                                     'date_added']))
@@ -183,6 +211,26 @@ def activate_shorten_url(id):
     try:
         action_to_perform = request.path.split("/")[4]
         shorten_url = ShortenUrl.query.get_or_404(id)
+        shorten_url.confirm_user()
+        if action_to_perform == "activate":
+            return shorten_url.activate()
+        return shorten_url.deactivate()
+    except NotFound:
+        return page_not_found("The resource you seek to update doesn't exist")
+
+@api.route('/api/shorten-urls/<shorten_url_name>/activate', methods=['PUT'])
+@api.route('/api/shorten-urls/<shorten_url_name>/deactivate', methods=['PUT'])
+@auth.login_required
+@permission
+def activate_shorten_url_with_name(shorten_url_name):
+    """
+    this function activates or deactivates a shorten_url based on the
+    shorten_url name
+    """
+    try:
+        action_to_perform = request.path.split("/")[4]
+        shorten_url = ShortenUrl.query.filter_by(
+            shorten_url_name=shorten_url_name).first_or_404()
         shorten_url.confirm_user()
         if action_to_perform == "activate":
             return shorten_url.activate()
