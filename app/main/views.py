c8cc19ac499c14fccce8f6aa0dd744bf07ac0ac6
koya adegboyega <adegboyega.koya@andela.com>
2017-04-10 13:27:37 +0100

    [Feature] Creates index and homepage endpoints

--- app/main/views.py
+++ app/main/views.py
@@ -12,16 +12,26 @@ from .forms import RegistrationForm, LoginForm
 @main.route('/login/', strict_slashes=False)
 def index():
     # renders the index page when any of the above route is reached
+@main.route('/')
+@main.route('/index/')
+@main.route('/register/')
+@main.route('/login/')
+def index():
     register_form = RegistrationForm()
     login_form = LoginForm()
     context = {"title": "Url Shortener", "registerForm": register_form,
                "loginForm": login_form}
     return render_template("index.html", context=context)
 
-
 @main.route('/dashboard/', methods=["GET", "POST"], strict_slashes=False)
 @login_required
 def dashboard():
     # logout_user()
+    return render_template('homepage.html')
+
+@main.route('/dashboard/', methods=["GET", "POST"])
+@login_required
+def dashboard():
+    logout_user()
     return render_template('homepage.html')
 
